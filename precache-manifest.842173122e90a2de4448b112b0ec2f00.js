0b71ad138caea4c4ac43c4f07b00f3987ecd7b39
Diya-Dangol <diya.dangol15@gmail.com>
2021-01-17 11:41:30 +0545

    Updates

---
+++ precache-manifest.842173122e90a2de4448b112b0ec2f00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "843b5154686453edd9e8242b4a77e8fd",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "aeaeb6dfacbc02f52667",
+    "url": "/recipe-app/static/css/main.9d4b2735.chunk.css"
+  },
+  {
+    "revision": "0927917df672cdbd6256",
+    "url": "/recipe-app/static/js/2.6eb5bad2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-app/static/js/2.6eb5bad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aeaeb6dfacbc02f52667",
+    "url": "/recipe-app/static/js/main.bbc72147.chunk.js"
+  },
+  {
+    "revision": "cd77bb1956273d314d45",
+    "url": "/recipe-app/static/js/runtime-main.14680ac4.js"
+  }
+]);
